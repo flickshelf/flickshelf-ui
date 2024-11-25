1a909125f626b06d576a1174e12b9e064a3cf817
Sabrina Rodrigues <sabrinnasrodrigues@gmail.com>
2024-11-24 20:58:11 -0500

    start Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,25 @@
+import style from "./Header.module.css"
+
 export function Header() {
-    return <h1>FlickShelf</h1>
+    return (
+		<header className={style.websiteHeader}>
+			<a onClick="openRegisterPage()"><img id="logotipo" src="png/temp-logo.png" alt="FlickShelf logo"/></a>
+
+			<div className={style.darkBlueHeader}>
+				<div className={style.logo}>
+				</div>
+				<nav id="navigation">
+				<ul>
+					<input className={style.registerBtn} type="button" value="Register" onClick="openRegisterPage()"/>
+					<input className={style.mListBtn} type="button" value="Movies list" onClick="openListMoviesPage()"/>
+					<input className={style.sListBtn} type="button" value="Series list" onClick="openListSeriesPage()"/>  
+					<div className={style.InputContainer}>
+					<input className={style.logoutBtn} type="button" onClick="logout()" value=""/>
+					<ion-icon name="log-out-outline" className={style.inputIcon}></ion-icon>
+					</div>
+				</ul>
+				</nav>
+			</div>
+		</header>
+    )
 }
