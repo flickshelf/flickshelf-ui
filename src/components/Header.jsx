04c9047c3007657c6962275065cf1dabd3df0ec6
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-11-26 23:00:04 -0300

    finish header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,38 @@
+import { LogOutOutline } from 'react-ionicons';
+
 import style from "./Header.module.css"
 
-export function Header() {
-    return (
-		<header className={style.websiteHeader}>
-			<a onClick="openRegisterPage()"><img id="logotipo" src="png/temp-logo.png" alt="FlickShelf logo"/></a>
+import logo from '../assets/temp-logo.png'
 
+export function Header() {
+	const openRegisterPage = () => {}
+
+	const openListMoviesPage = () => {}
+
+	const openListSeriesPage = () => {}
+
+	const logout = () => {}
+
+    return (
+		<header>
+			<a onClick={openRegisterPage}>
+				<img className={style.logotipo} src={logo} alt="FlickShelf logo" />
+			</a>
 			<div className={style.darkBlueHeader}>
-				<div className={style.logo}>
-				</div>
 				<nav id="navigation">
-				<ul>
-					<input className={style.registerBtn} type="button" value="Register" onClick="openRegisterPage()"/>
-					<input className={style.mListBtn} type="button" value="Movies list" onClick="openListMoviesPage()"/>
-					<input className={style.sListBtn} type="button" value="Series list" onClick="openListSeriesPage()"/>  
-					<div className={style.InputContainer}>
-					<input className={style.logoutBtn} type="button" onClick="logout()" value=""/>
-					<ion-icon name="log-out-outline" className={style.inputIcon}></ion-icon>
-					</div>
-				</ul>
+					<ul>
+						<input className={style.registerBtn} type="button" value="Register" onClick={openRegisterPage} />
+						<input className={style.mListBtn} type="button" value="Movies list" onClick={openListMoviesPage} />
+						<input className={style.sListBtn} type="button" value="Series list" onClick={openListSeriesPage} /> 
+
+						<button className={style.logoutBtn} onClick={logout}>
+							<LogOutOutline
+								title={'test'}
+								width="32px"
+								color
+							/>
+						</button>
+					</ul>
 				</nav>
 			</div>
 		</header>
