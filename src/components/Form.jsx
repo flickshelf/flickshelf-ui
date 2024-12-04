1909c8d13432bd55f4f5f0aa922505f5a17fdd7f
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-12-03 22:37:46 -0300

    add React State hook

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,84 @@
+import { useState } from 'react';
+
 import style from './Form.module.css'
 
 export function Form() {
+    const [serieTitle, setSerieTitle] = useState('')
+    const [serieGenre, setSerieGenre] = useState('')
+    const [serieSeasons, setSerieSeasons] = useState(0)
+    const [serieReleaseYear, setSerieReleaseYear] = useState(0)
+    const [serieSynopsis, setSerieSynopsis] = useState('')
+
     function saveFilledValues() {}
 
     const create = () => {
-        console.log('clicked create')
+        console.log(serieTitle, serieGenre, serieSeasons, serieReleaseYear, serieSynopsis)
+
+        // -------------------------------
+        
+        // if (
+        //     serieTitle.value === "" 
+        //     || serieGenre.value === "" 
+        //     || serieSeasons.value === "" 
+        //     || serieReleaseYear.value === "" 
+        //     || serieSynopsis.value === ""
+        // ) {
+        //     return alert("All informations needs to be filled!")
+        // } 
+
+        // const ownerId = localStorage.getItem('loggedUserId')
+
+        // disableButton()
+
+        // axios.post('https://api.flickshelf.com/series', {
+        //     ownerId,
+        //     serieTitle: serieTitle.value,
+        //     serieGenre: serieGenre.value,
+        //     serieSeasons: serieSeasons.value,
+        //     serieReleaseYear: serieReleaseYear.value,
+        //     serieSynopsis: serieSynopsis.value,
+        //     })
+        //     .then(() => {
+        //     alert(`Serie ${serieTitle.value} created successfully!`)
+        //     localStorage.removeItem('formValues')
+
+        //     serieTitle.value = ''
+        //     serieGenre.value = ''
+        //     serieSeasons.value = ''
+        //     serieReleaseYear.value = ''
+        //     serieSynopsis.value = ''
+        //     })
+        //     .catch(() => {
+        //     alert('There was an error. Try again.');
+        //     })
+        //     .finally(() => {
+        //     enableButton()
+        //     })
+    }
+
+    const onChangeTitle = (event) => {
+        const newTitle = event.target.value
+        setSerieTitle(newTitle)
+    }
+
+    const onChangeSeasons = (event) => {
+        const newSeasonsNumber = event.target.value
+        setSerieSeasons(newSeasonsNumber)
+    }
+
+    const onChangeReleaseYear = (event) => {
+        const newReleaseYear = event.target.value
+        setSerieReleaseYear(newReleaseYear)
+    }
+
+    const onChangeSerieSynopsis = (event) => {
+        const newSerieSynopsis = event.target.value
+        setSerieSynopsis(newSerieSynopsis)
+    }
+
+    const onChangeSerieGenre = (event) => {
+        const newSerieGenre = event.target.value
+        setSerieGenre(newSerieGenre)
     }
 
     return (
@@ -13,26 +87,28 @@ export function Form() {
                 <h2 className={style.title}>Create your serie</h2>
                 <label htmlFor="serie-title">Title</label>
                 <input 
-                    id="serie-title"
                     type="text" 
+                    value={serieTitle}
+                    onChange={onChangeTitle}
                     placeholder="Type series title..."
                     onBlur={saveFilledValues()}
                 />
                 
                 <label htmlFor="serie-genre">Genre</label>
-                <select id="serie-genre" onChange={saveFilledValues()}>
-                <option value="">Select genre</option>
-                <option value="comedy">Comedy</option>
-                <option value="sitcom">Sitcom</option>
-                <option value="scifi">Sci/Fi</option>
-                <option value="horror">Horror</option>
-                <option value="drama">Drama</option>
+                <select value={serieGenre} onChange={onChangeSerieGenre}>
+                    <option value="">Select genre</option>
+                    <option value="comedy">Comedy</option>
+                    <option value="sitcom">Sitcom</option>
+                    <option value="scifi">Sci/Fi</option>
+                    <option value="horror">Horror</option>
+                    <option value="drama">Drama</option>
                 </select>
     
                 <label htmlFor="serie-seasons">Seasons</label>
                 <input 
-                    id="serie-seasons"
                     type="number" 
+                    value={serieSeasons}
+                    onChange={onChangeSeasons}
                     placeholder="Total seasons number"
                     onBlur={saveFilledValues()}
                     min="1"
@@ -41,8 +117,9 @@ export function Form() {
     
                 <label htmlFor="serie-release-year">Release year</label>
                 <input 
-                    id="serie-release-year"
                     type="number" 
+                    value={serieReleaseYear}
+                    onChange={onChangeReleaseYear}
                     placeholder="Serie release year"
                     onBlur={saveFilledValues()}
                     min="1957"
@@ -53,6 +130,8 @@ export function Form() {
                 <textarea 
                     id="serie-synopsis" 
                     name="Synopsis" 
+                    value={serieSynopsis}
+                    onChange={onChangeSerieSynopsis}
                     placeholder="Type serie synopsis..."
                     onBlur={saveFilledValues()}
                 ></textarea>
