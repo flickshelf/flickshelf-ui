8acee6d43a05967cb617e0afe7b75254444f82ae
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-12-22 17:40:00 -0300

    improve `Card` component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,24 @@
-return function Card() { 
-    <div className="serie-item" id="serie.id">
-        <h3 className="serie-title">serie.title</h3>
-        <p>Genre: ${handleSerieGenre(serie.genre)}</p>      
-        <p>Season: ${serie.seasons}</p>  
-        <p>Release year: ${serie.release_year}</p>  
+import style from './Card.module.css';
 
-        <div className="synopsis-div">
-            <p>Synopsis: ${serie.synopsis}</p>
+export function Card(props) {
+    const { serie } = props;
+
+    return (
+        <div className={style.serieItem}>
+            <h3 className={style.serieTitle}>{serie.title}</h3>
+
+            {/* <p>Genre: ${handleSerieGenre(serie.genre)}</p> */}
+            <p>Seasons: {serie.seasons}</p>
+            <p>Release year: {serie.release_year}</p>
+
+            <div className="synopsis-div">
+                <p>Synopsis: {serie.synopsis}</p>
+            </div>
+
+            <div className="card-buttons">
+                {/* <ion-icon name="create-outline" onclick="handleEditClick('${serie.id}')"></ion-icon> */}
+                {/* <ion-icon name="trash" onclick="handleTrashClick('${serie.id}')"></ion-icon> */}
+            </div>
         </div>
-
-       <div className="card-buttons">
-        <ion-icon name="create-outline" onclick="handleEditClick('${serie.id}')"></ion-icon>
-        <ion-icon name="trash" onclick="handleTrashClick('${serie.id}')"></ion-icon>
-       </div>
-    </div>
-} 
+    )
+}
