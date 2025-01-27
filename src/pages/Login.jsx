17d0395c0379f5640595fecf9a4783796ca70508
Gabriel Klein <gabrielkleinribas@gmail.com>
2025-01-26 23:21:18 -0300

    add `login` function

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,71 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import style from './Login.module.css'
 
+import axios from 'axios';
+
 export function Login() {
+    const navigate = useNavigate()
+    
+    const [isSignIn, setIsSignIn] = useState(false);
+
+    const [loginEmail, setLoginEmail] = useState('');
+    const [loginPassword, setLoginPassword] = useState('');
+
+    const [signupEmail, setSignupEmail] = useState('');
+
+    const handleSignInSignUpClick = () => {
+        setIsSignIn(!isSignIn)
+    }
+
+    const handleLoginEmail = (event) => {
+        setLoginEmail(event.target.value)
+        console.log(loginEmail)
+    }
+
+    const handleLoginPassword = (event) => {
+        setLoginPassword(event.target.value)
+        console.log(loginPassword)
+    }
+
+    const login = () => {
+        // disableLoginButton({ button: 'login' })
+        // showLoginLoadingSpinner({ button: 'login' })
+
+        axios.post('https://api.flickshelf.com/login', {
+            loginEmail,
+            loginPassword
+        }).then((res) => {
+            console.log(res.data)
+            const userId = res.data
+
+            if (userId) {
+                // storeUserCredentialsOnBrowser(userId)
+                
+                console.log('navigate("/")')
+                return navigate("/")
+            } else {
+                // unableLoginButton({button: 'login'})
+                alert('[ERROR]: Invalid email or password. Try again.')
+            }
+        })
+        .catch((err) => {
+            console.error(err)
+        })
+    }
+
     return (
-        <div className={style.container}>
+        <div className={`${style.container} ${isSignIn ? style.signInJs : style.signUpJs}`}>
             <div className={`${style.content} ${style.firstContent}`}>
                 <div className={style.firstColumn}>
                     <h2 className={`${style.title} ${style.titlePrimary}`}>Welcome Back!</h2>
                     <p className={style.descriptionPrimary}>To keep connected with us</p>
                     <p className={style.descriptionPrimary}>please login with your personal info.</p>  
-                    <button id="signin" className={`${style.btn} ${style.btnPrimary}`}>Sign in</button>
+                    <button 
+                        id="signin" 
+                        className={`${style.btn} ${style.btnPrimary}`}
+                        onClick={handleSignInSignUpClick}
+                    >Sign in</button>
                 </div> 
     
                 <div className={style.secondColumn}>
@@ -69,7 +126,12 @@ export function Login() {
     
                         </label>                    
                     
-                        <button type="button" className={`${style.btn} ${style.btnSecond}`} onclick="signUp()" id="sign-up-button">Sign up</button>
+                        <button 
+                            type="button" 
+                            className={`${style.btn} ${style.btnSecond}`} 
+                            onclick="signUp()" 
+                            id="sign-up-button"
+                        >Sign up</button>
                        
                     </form>
                 </div>
@@ -80,7 +142,11 @@ export function Login() {
                     <h2 className={`${style.title} ${style.titlePrimary}`}>Hello, friend!</h2>
                     <p className={style.descriptionPrimary}>Enter your personal details</p>
                     <p className={style.descriptionPrimary}>and start a journey with us.</p>  
-                    <button id="signup" className={`${style.btn} ${style.btnPrimary}`}>Sign up</button>
+                    <button 
+                        id="signup" 
+                        className={`${style.btn} ${style.btnPrimary}`}
+                        onClick={handleSignInSignUpClick}
+                    >Sign up</button>
                 </div> 
     
                 <div className={style.secondColumn}>
@@ -106,19 +172,35 @@ export function Login() {
                     <form className={style.form}>
                         <label className={style.labelInput} for="">
                             <i className={`${style.faRegular} ${style.faEnvelope} ${style.iconModify}`}></i>
-                            <input type="email" placeholder="Email" id="login-email"/>
+                            <input 
+                                type="email" 
+                                placeholder="Email" 
+                                id="login-email"
+                                onChange={handleLoginEmail}
+                            />
                         </label>
     
                         <label className={style.labelInput} for="">
                             <i className={`${style.faSolid} ${style.faLock} ${style.iconModify}`}></i>
-                            <input type="password" id="password-left" placeholder="Password" autocomplete="new-password"/>
+                            <input 
+                                type="password" 
+                                id="password-left" 
+                                placeholder="Password" 
+                                autocomplete="new-password"
+                                onChange={handleLoginPassword}
+                            />
     
                             <div className={style.showPassword}>
                                 <i className={`${style.faSolid} ${style.faEyeSlash}`} id="show-password-left" onclick="showPasswordSignIn()"></i>   
                             </div>
                         </label> 
                         <a className={style.password} href="#">Forgot your password?</a>
-                        <button type="button" className={`${style.btn} ${style.btnSecond}`} onclick="login()" id="login-button">Sign in</button>
+                        <button 
+                            type="button" 
+                            className={`${style.btn} ${style.btnSecond}`} 
+                            onClick={login}
+                            id="login-button"
+                        >Sign in</button>
                     </form>
                 </div>
             </div>
