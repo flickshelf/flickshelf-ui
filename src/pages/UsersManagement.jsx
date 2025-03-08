8676803e043f21d18fe02824d16e83f8a8edb3d2
Gabriel Klein <gabrielkleinribas@gmail.com>
2025-03-08 05:36:10 +0000

    add user card on management page and adjust list

--- src/pages/UsersManagement.jsx
+++ src/pages/UsersManagement.jsx
@@ -1,14 +1,25 @@
+import { useState } from 'react';
+
 import style from './UsersManagement.module.css'
 
 import { Header } from '../components/Header'
+import { UserCard } from '../components/UserCard'
 
 export const UsersManagement = () => {
+    const [users, setUsers] = useState([]);
+
     return (
         <>
             <Header />
 
             <div className={style.usersManagementGeneralContainer}>
                 <h2 className={style.pageTitle}>Users Management</h2>
+
+                <div className={style.usersList}>
+                    { users.map((user) => {
+                        return <UserCard />
+                    }) }
+                </div>
             </div>
         </>
     )
