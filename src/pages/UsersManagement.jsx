50ba28b401eeb45a2d969b567c41556bab9b9b18
Gabriel Klein <gabrielkleinribas@gmail.com>
2025-03-08 05:38:16 +0000

    add `getAllUsers` function

--- src/pages/UsersManagement.jsx
+++ src/pages/UsersManagement.jsx
@@ -1,12 +1,32 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+import axios from 'axios';
 
 import style from './UsersManagement.module.css'
 
 import { Header } from '../components/Header'
 import { UserCard } from '../components/UserCard'
 
+// const apiUrl = 'https://api.flickshelf.com'
+const apiUrl = 'http://localhost:3333'
+
 export const UsersManagement = () => {
     const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        getAllUsers()
+    }, [])
+
+    function getAllUsers() {
+        axios.get(`${apiUrl}/users`)
+            .then((allUsers) => {
+                setUsers(allUsers.data)
+            })
+            .catch((error) => {
+                console.error(error)
+                alert('There was an error on getting users. Try again.')
+            })
+    }
 
     return (
         <>
