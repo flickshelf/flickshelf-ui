f99d6a10f1b8352aa83284eb1edaaaa9b79eac20
Sabrina Rodrigues <sabrinnasrodrigues@gmail.com>
2025-04-12 20:47:21 -0400

    add modal content

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -20,6 +20,16 @@ export function UserCard (props) {
         )
     }
 
+    const userName = "2"
+
+    const userEmail = "2"
+
+    const userRole = "2"
+
+    const onChangeName = () => {
+
+    }
+
     return (
         <div className={style.userCard}>
             <div className={style.leftItems}>
@@ -44,6 +54,33 @@ export function UserCard (props) {
                             <Dialog.Content className={style.modalContent}>
                                 <Dialog.Title>User Update</Dialog.Title>
                                 
+                                <label className={style.label}>Name</label>
+                                <input 
+                                    type="text" 
+                                    value={userName}
+                                    onChange={onChangeName}
+                                    placeholder='Type your name...'
+                                />
+
+                                <label className={style.label}>Email</label>
+                                <input 
+                                    type="text" 
+                                    value={userEmail}
+                                    onChange={onChangeName}
+                                    placeholder='Type your email...'
+                                />
+
+                                <label className={style.label}>Role</label>
+                                <select value={userRole} onChange={onChangeName}>
+                                    <option value="">Select role</option>
+                                    <option value="Admin">Admin</option>
+                                    <option value="User">User</option>
+                                </select>
+
+                                <button className={style.cancelButton}>Cancel</button>
+
+                                <button className={style.updateButton}>Update</button>
+
                                 <Dialog.Close className={style.modalCloseButton}>
                                     <IconContext.Provider value={{ className: style.modalCloseIcon }}>
                                         <IoClose title={'Close modal'} />
