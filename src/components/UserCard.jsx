098fda4fdee6fb0085addb467164b6f0365f80ef
Gabriel Klein <gabrielkleinribas@gmail.com>
2025-04-12 22:23:16 -0300

    update user update dialog content section

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,9 +1,10 @@
 import style from './UserCard.module.css';
 
 import * as Dialog from '@radix-ui/react-dialog';
+import { UserUpdateDialog } from '../dialogs/UserUpdateDialog';
 
 import { IconContext } from "react-icons";
-import { IoPencil, IoTrash, IoClose } from "react-icons/io5";
+import { IoPencil, IoTrash } from "react-icons/io5";
 
 import loadingSpinner from '../assets/spinner.gif'
 
@@ -18,16 +19,6 @@ export function UserCard (props) {
                 </div>
             </div>
         )
-    }
-
-    const userName = "2"
-
-    const userEmail = "2"
-
-    const userRole = "2"
-
-    const onChangeName = () => {
-
     }
 
     return (
@@ -49,44 +40,7 @@ export function UserCard (props) {
                             </button>
                         </Dialog.Trigger>
                         <Dialog.Portal>
-                            <Dialog.Overlay className={style.modalOverlay} />
-
-                            <Dialog.Content className={style.modalContent}>
-                                <Dialog.Title>User Update</Dialog.Title>
-                                
-                                <label className={style.label}>Name</label>
-                                <input 
-                                    type="text" 
-                                    value={userName}
-                                    onChange={onChangeName}
-                                    placeholder='Type your name...'
-                                />
-
-                                <label className={style.label}>Email</label>
-                                <input 
-                                    type="text" 
-                                    value={userEmail}
-                                    onChange={onChangeName}
-                                    placeholder='Type your email...'
-                                />
-
-                                <label className={style.label}>Role</label>
-                                <select value={userRole} onChange={onChangeName}>
-                                    <option value="">Select role</option>
-                                    <option value="Admin">Admin</option>
-                                    <option value="User">User</option>
-                                </select>
-
-                                <button className={style.cancelButton}>Cancel</button>
-
-                                <button className={style.updateButton}>Update</button>
-
-                                <Dialog.Close className={style.modalCloseButton}>
-                                    <IconContext.Provider value={{ className: style.modalCloseIcon }}>
-                                        <IoClose title={'Close modal'} />
-                                    </IconContext.Provider>
-                                </Dialog.Close>
-                            </Dialog.Content>
+                            <UserUpdateDialog />
                         </Dialog.Portal>
 
                     </Dialog.Root>
