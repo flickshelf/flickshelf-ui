102d44fad7b306e648f5a0f1c3ef8a0708b37097
Gabriel Klein <gabrielkleinribas@gmail.com>
2025-06-29 11:06:58 -0300

    add fallback in case of no posterUrl

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,6 +7,36 @@ const posterBasePath = import.meta.env.VITE_EXTERNAL_API_IMAGES_URL
 export function Card(props) {
     const { serie, isLoading, onClick } = props;
 
+    function handleSerieGenre(genre) {
+      let genreToDisplay = ''
+    
+      switch (genre) {
+        case 'comedy':
+          genreToDisplay = 'Comedy'
+          break;
+        case 'sitcom':
+          genreToDisplay = 'Sitcom'
+          break;
+        case 'scifi':
+          genreToDisplay = 'Sci/Fi'
+          break;
+        case 'horror':
+          genreToDisplay = 'Horror'
+          break;
+        case 'drama':
+          genreToDisplay = 'Drama'
+          break;
+        case 'crime':
+          genreToDisplay = 'Crime'
+          break;
+        default:
+          genreToDisplay = '-'
+          break;
+      }
+      
+      return genreToDisplay
+  }
+
     if (isLoading.active && isLoading.cardId === serie.id) {
       return (
         <div className={style.serieItem}>
@@ -24,6 +54,19 @@ export function Card(props) {
           onClick={onClick}
         >
             <h3 className={style.serieTitle}>{serie.title}</h3>
+
+            { (!serie.posterUrl || !posterBasePath) && 
+              <>
+                <h3 className={style.serieTitle}>{serie.title}</h3>
+                <p>Genre: {handleSerieGenre(serie.genre)}</p>
+                <p>Seasons: {serie.seasons}</p>
+                <p>Release year: {serie.release_year}</p>
+
+                <div className={style.synopsisDiv}>
+                    <p>Synopsis: {serie.synopsis}</p>
+                </div>
+              </>
+            }
         </div>
     )
 }
