e7f9e8ea876b43351f304fb41bfe85c3bec71449
Gabriel Klein <gabrielkleinribas@gmail.com>
2025-06-29 02:36:13 -0300

    update serie card style and add background poster

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,39 +1,11 @@
 import style from './Card.module.css';
 
-import { IconContext } from "react-icons";
-import { IoPencil, IoTrash } from "react-icons/io5";
-
 import loadingSpinner from '../assets/spinner.gif';
 
-export function Card(props) {
-    const { serie, onUpdate, onDelete, isLoading } = props;
+const posterBasePath = import.meta.env.VITE_EXTERNAL_API_IMAGES_URL
 
-    function handleSerieGenre(genre) {
-        let genreToDisplay = ''
-      
-        switch (genre) {
-          case 'comedy':
-            genreToDisplay = 'Comedy'
-            break;
-          case 'sitcom':
-            genreToDisplay = 'Sitcom'
-            break;
-          case 'scifi':
-            genreToDisplay = 'Sci/Fi'
-            break;
-          case 'horror':
-            genreToDisplay = 'Horror'
-            break;
-          case 'drama':
-            genreToDisplay = 'Drama'
-            break;
-          default:
-            genreToDisplay = '-'
-            break;
-        }
-        
-        return genreToDisplay
-    }
+export function Card(props) {
+    const { serie, isLoading, onClick } = props;
 
     if (isLoading.active && isLoading.cardId === serie.id) {
       return (
@@ -46,29 +18,12 @@ export function Card(props) {
     }
 
     return (
-        <div className={style.serieItem}>
+        <div 
+          className={style.serieItem} 
+          style={{ backgroundImage: `url(${posterBasePath}${serie.posterUrl})` }}
+          onClick={onClick}
+        >
             <h3 className={style.serieTitle}>{serie.title}</h3>
-
-            <p>Genre: {handleSerieGenre(serie.genre)}</p>
-            <p>Seasons: {serie.seasons}</p>
-            <p>Release year: {serie.release_year}</p>
-
-            <div className={style.synopsisDiv}>
-                <p>Synopsis: {serie.synopsis}</p>
-            </div>
-
-            <div className={style.cardButtons}>
-              <IconContext.Provider value={{ className: style.cardIcon }}>
-                  <IoPencil 
-                    onClick={() => onUpdate(serie.id)}
-                    title={'Update'}
-                  />
-                  <IoTrash 
-                    onClick={() => onDelete(serie.id)}  
-                    title={'Delete'}
-                  />
-              </IconContext.Provider>
-            </div>
         </div>
     )
 }
