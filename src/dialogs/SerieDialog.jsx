127a99694dcf13f15e2ab1f0d869ea870a11580f
Gabriel Klein <gabrielkleinribas@gmail.com>
2025-06-29 02:33:44 -0300

    add serie dialog action buttons and format genre function

--- src/dialogs/SerieDialog.jsx
+++ src/dialogs/SerieDialog.jsx
@@ -3,12 +3,43 @@ import React from 'react';
 import * as Dialog from '@radix-ui/react-dialog';
 
 import { IconContext } from 'react-icons';
+import { IoPencil, IoTrash } from "react-icons/io5";
 import { IoClose } from 'react-icons/io5';
 
 import style from './SerieDialog.module.css';
 
 function SerieDialogComponent (props, ref) {
-    const { serie } = props;
+    const { serie, onUpdate, onDelete } = props;
+
+    function handleSerieGenre(genre) {
+        let genreToDisplay = ''
+      
+        switch (genre) {
+          case 'comedy':
+            genreToDisplay = 'Comedy'
+            break;
+          case 'sitcom':
+            genreToDisplay = 'Sitcom'
+            break;
+          case 'scifi':
+            genreToDisplay = 'Sci/Fi'
+            break;
+          case 'horror':
+            genreToDisplay = 'Horror'
+            break;
+          case 'drama':
+            genreToDisplay = 'Drama'
+            break;
+          case 'crime':
+            genreToDisplay = 'Crime'
+            break;
+          default:
+            genreToDisplay = '-'
+            break;
+        }
+        
+        return genreToDisplay
+    }
 
     return (
         <>
@@ -31,6 +62,18 @@ function SerieDialogComponent (props, ref) {
                         <div className={style.synopsisDiv}>
                             <p>Synopsis: {serie.synopsis}</p>
                         </div>
+                    </div>
+                    <div className={style.cardButtons}>
+                        <IconContext.Provider value={{ className: style.cardIcon }}>
+                            <IoPencil 
+                                onClick={() => onUpdate(serie.id)}
+                                title={'Update'}
+                            />
+                            <IoTrash 
+                                onClick={() => onDelete(serie.id)}  
+                                title={'Delete'}
+                            />
+                        </IconContext.Provider>
                     </div>
                 </div>
                 <Dialog.Close autoFocus className={style.modalCloseButton}>
